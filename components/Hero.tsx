50d989bda08cec741a3efe7506f3c40358397ce4
jehianth <jehianbusiness@gmail.com>
2023-06-11 18:27:09 +0700

    fix: hero title overflow (#3)

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 const Hero = () => {
     return (
         <section id='hero' className='bg-bghero w-full lg:h-[60vh] text-white'>
-            <div className='translate-y-[525px] translate-x-10 lg:pl-40 lg:pt-24 w-[490px] lg:w-[814px]'>
-                <h1 className='text-5xl font-bold lg:font-extrabold lg:text-6xl lg:tracking-wide lg:leading-tight'>Desain Arsitektur Modern dengan Inovasi Lingkungan Terintegrasi</h1>
+            <div className='translate-y-[525px] translate-x-10 lg:pl-40 lg:pt-24 w-[470px] lg:w-[814px]'>
+                <h1 className='text-[46px] font-bold lg:font-extrabold lg:text-6xl lg:tracking-wide leading-tight'>Desain Arsitektur Modern dengan Inovasi Lingkungan Terintegrasi</h1>
             </div>
             <div className='bg-bghero w-full -z-10 absolute inset-0 transform skew-y-[5deg] -translate-y-32'></div>
             <button className='translate-y-[550px] translate-x-96 lg:translate-x-52 lg:translate-y-5 transition duration-500 hover:scale-110'>
