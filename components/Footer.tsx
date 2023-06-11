362820488a54f2a0b142b271ed0d43a4740226d4
jehianth <jehianbusiness@gmail.com>
2023-06-11 10:54:14 +0700

    fix: footer change medium based to xl based

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,14 +4,14 @@ import Link from 'next/link'
 
 const Footer = () => {
     return (
-        <footer id='kontak' className='bg-bgfooter w-full p-5 md:p-16 flex flex-col md:flex-row text-textfooter'>
-            <div className='md:flex md:flex-row'>
-                <Image src='/harita.jpg' width={537} height={288} alt='Harita' className='mx-auto md:pt-0 md:h-[306px] md:w-[221px] md:object-none'/>
-                <div className='md:flex md:flex-col md:ml-10 md:w-[261px]'>
-                    <Image src='/logo-white.png' width={166} height={45.16} alt='Logo JBKons' className='mx-auto mt-10 mb-5 md:ml-auto md:mb-3 md:mt-6' />
+        <footer id='kontak' className='bg-bgfooter w-full p-5 xl:p-16 flex flex-col xl:flex-row text-textfooter'>
+            <div className='xl:flex xl:flex-row'>
+                <Image src='/harita.jpg' width={537} height={288} alt='Harita' className='mx-auto xl:pt-0 xl:h-[306px] xl:w-[221px] xl:object-none'/>
+                <div className='xl:flex xl:flex-col xl:ml-10 xl:w-[261px]'>
+                    <Image src='/logo-white.png' width={166} height={45.16} alt='Logo JBKons' className='mx-auto mt-10 mb-5 xl:ml-auto xl:mb-3 xl:mt-6' />
                     <p className='font-semibold text-center text-sm'>KONSULTAN PERENCANAAN DAN PENGAWASAN</p>
                     <p className='text-justify mt-6 text-sm text-zinc-400'>Percayakan desain bangunan pada JBKons, konsultan arsitektur modern dengan sentuhan alam yang brilian untuk hasil yang berkesan!</p>
-                    <div className='mx-auto md:ml-0 flex flex-row justify-center items-center'>
+                    <div className='mx-auto xl:ml-0 flex flex-row justify-center items-center'>
                         <Link href='https://www.instagram.com/jbkons.id/' target='_blank'>
                             <button className='mt-5 grayscale opacity-80 hover:grayscale-0 hover:opacity-100 transition duration-700 hover:scale-105'>
                                 <Image src='/ig.png' width={108} height={40} alt='Instagram JBKons' />
@@ -25,12 +25,12 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            <div className='mt-10 md:ml-20 md:mt-6 md:w-[800px]'>
+            <div className='mt-10 xl:ml-20 xl:mt-6 xl:w-[800px]'>
                 <p className='font-semibold text-2xl border-b-2 border-purple-700 w-fit mb-6'>KONTAK KAMI</p>
-                <p className='text-sm text-justify md:text-start hover:text-purple-700 border-l-2 border-purple-700 pl-4 mb-3'>Alamat : Jalan Gerilya Gang II Nomor 26 Purwokerto, Kel. Tanjung, Kec. Purwokerto Selatan, Kab. Banyumas, Prov. Jawa Tengah 53144</p>
+                <p className='text-sm text-justify xl:text-start hover:text-purple-700 border-l-2 border-purple-700 pl-4 mb-3'>Alamat : Jalan Gerilya Gang II Nomor 26 Purwokerto, Kel. Tanjung, Kec. Purwokerto Selatan, Kab. Banyumas, Prov. Jawa Tengah 53144</p>
                 <p className='text-sm hover:text-purple-700 border-l-2 border-purple-700 pl-4 mb-3'>No. Telepon : (+62) 82225314338</p>
                 <p className='text-sm hover:text-purple-700 border-l-2 border-purple-700 pl-4 mb-3'>Email : pt.jbkons@gmail.com</p>
-                <p className='text-sm mt-10 text-zinc-400 text-center md:text-start'>© 2023 PT. Joglomas Brilian Konsultan. All Rights Reserved.</p>
+                <p className='text-sm mt-10 text-zinc-400 text-center xl:text-start'>© 2023 PT. Joglomas Brilian Konsultan. All Rights Reserved.</p>
             </div>
         </footer>
     )
