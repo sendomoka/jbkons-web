e0b9c0255473ee82cf3d810c33804dac9661808f
jehianth <jehianbusiness@gmail.com>
2023-06-11 18:48:58 +0700

    fix: hero title overflow (#7)

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Hero = () => {
     return (
         <section id='hero' className='bg-bghero w-full lg:h-[60vh] text-white'>
-            <div className='translate-y-[480px] lg:translate-y-0 translate-x-16 lg:pl-40 lg:pt-24 w-[400px] lg:w-[814px]'>
+            <div className='translate-y-[480px] lg:translate-y-0 translate-x-10 lg:pl-40 lg:pt-24 w-[400px] lg:w-[814px]'>
                 <h1 className='text-[39.6px] font-bold lg:font-extrabold lg:text-6xl lg:tracking-wide leading-tight lg:leading-tight'>Desain Arsitektur Modern dengan Inovasi Lingkungan Terintegrasi</h1>
             </div>
             <div className='bg-bghero w-full -z-10 absolute inset-0 transform skew-y-[5deg] -translate-y-44 lg:-translate-y-32'></div>
